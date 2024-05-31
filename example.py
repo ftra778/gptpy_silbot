d94ea337d3d9603c3ceab9243fefa4fdb4ca12f0
ftra778 <79771841+ftra778@users.noreply.github.com>
2024-05-31 16:59:52 +1200

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,4 @@
 from gptpy3.chat_client import ChatClient
 
-client = ChatClient(name='gpt', openai_key_path="/home/user1/secret-key.txt", host='192.168.0.102', port=7788)
+client = ChatClient(name='gpt', openai_key_path="/home/user1/secret-key.txt", host='192.168.0.100', port=7788)
 client.run()
-# 172.23.38.211
